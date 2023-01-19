9ea1392a5146b3c963edbd9ec23e2ad3f8c1ef00
david <davidsandoval92@outlook.com>
2023-01-18 23:08:31 -0600

    add fake data

---
+++ src/config/userData.js
@@ -0,0 +1,86 @@
+export const userData = {
+    "name": "David", 
+    "lastName": "Sandoval", 
+    "id": "93c28064-3cdb-4dc6-abcf-b810b5101955",
+    "email": "david@test.com",
+    "isAdmin": true
+}
+
+export const data = [
+    {
+        "id": "736872a1-ca45-45ca-a9aa-50d378712bff",
+        "firstName": "Angy",
+        "lastName": "Sissons",
+        "secondLastName": "Melhuish",
+        "email": "amelhuish0@intel.com",
+        "partialPayment": 4,
+        "estatusPayment": true,
+        "mount": "$100.00",
+        "lastPayment": "$25.00",
+        "dateLastPayment": "2022-08-13T22:02:55Z",
+        "createdAt": "2022-01-18T21:05:15Z",
+        "createdBy": "591270a2-e1c8-44d2-a843-14427e2c0fb4",
+        "updatedBy": "6a26c96e-bfd9-4e2f-b0af-a590295da8c5",
+        "updatedAt": "2023-01-02T15:32:35Z"
+    },
+    {
+        "id": "79d87df2-8cc4-4473-885e-1aee95d4df56",
+        "firstName": "Alane",
+        "lastName": "MacLaig",
+        "secondLastName": "Scamerden",
+        "email": "ascamerden1@webmd.com",
+        "partialPayment": 3,
+        "estatusPayment": false,
+        "mount": "$75.00",
+        "lastPayment": "$25.00",
+        "dateLastPayment": "2022-04-11T05:28:19Z",
+        "createdAt": "2021-12-02T19:58:35Z",
+        "createdBy": "3d23ae7f-f717-4a4c-8886-79b8bd6eb388",
+        "updatedBy": "08b50790-7257-4b5f-8ba7-6e9f6f7ff11e",
+        "updatedAt": "2023-01-10T13:24:16Z"
+    },
+    {
+        "id": "3ffcb090-f740-41ba-826d-dde622b0836c",
+        "firstName": "Lynnett",
+        "lastName": "Antram",
+        "secondLastName": "Obert",
+        "email": "lobert2@sohu.com",
+        "partialPayment": 4,
+        "estatusPayment": true,
+        "mount": "$100.00",
+        "lastPayment": "$25.00",
+        "dateLastPayment": "2022-06-07T00:58:06Z",
+        "createdAt": "2022-01-04T07:12:56Z",
+        "createdBy": "eb1c3c2a-cfb9-42e5-b7b7-3a9678f203b2",
+        "updatedBy": "78199120-bba1-4ad6-812d-aba6379e126c",
+        "updatedAt": "2023-01-08T22:13:03Z"
+    },
+    {
+        "id": "6348f7d4-35d1-4e2a-b3a9-932d50d4fee4",
+        "firstName": "Jyoti",
+        "lastName": "Tearle",
+        "secondLastName": "Rozanski",
+        "email": "jrozanski3@amazon.co.jp",
+        "partialPayment": 2,
+        "estatusPayment": false,
+        "mount": "$50.00",
+        "lastPayment": "$25.00",
+        "dateLastPayment": "2022-07-27T09:46:48Z",
+        "createdAt": "2022-01-30T09:21:17Z",
+        "createdBy": "7e0f7b8c-9a9d-48aa-b2e9-d73de45a348a",
+        "updatedBy": "c26de1e5-5b55-4e0c-abe6-88887e50d9c6",
+        "updatedAt": "2023-01-08T02:29:54Z"
+    },
+]
+
+// const array = [
+//     { id: 'A', pet: 'dog' },
+//     { id: 'B', pet: 'cat' },
+//   ];
+//   const filteredData = filter(array, {
+//     limit: 1,
+//     keywords: 'Do', // search for any field that contains the "Do" string
+//     includePaths,
+//     excludedPaths: [],
+//     caseSensitive: true,
+//   });
